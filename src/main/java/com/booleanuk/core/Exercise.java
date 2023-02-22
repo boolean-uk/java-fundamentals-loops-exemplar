1078385c0b749d671b6a163d010c42b5b93a7d2a
Lewis Campbell <ljoncb@gmail.com>
2023-02-22 16:13:21 +0000

    core and extension

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -11,35 +11,45 @@ public class Exercise {
 
     public void stepOne() {
         // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
-
-
+        numsZeroToThree = new int[4];
+        for(int i = 0; i < numsZeroToThree.length; i++) {
+            numsZeroToThree[i] = i;
+        }
     }
 
     public void stepTwo() {
         // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-
-
+        numsFiveToTen = new int[6];
+        for(int i = 0, j=5; i < numsFiveToTen.length; i++, j++) {
+            numsFiveToTen[i] = j;
+        }
     }
 
     public void stepThree() {
         // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-
-
+        countdown = new int[4];
+        for(int i = 0, j = 3; i < countdown.length; i++, j--) {
+            countdown[i] = j;
+        }
     }
 
     public boolean stepFour(int num) {
         // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array
-
-
-
+        for(int i = 0; i < favouriteNumbers.length; i++) {
+            if(favouriteNumbers[i] == num) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean stepFive(String hobby) {
         // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-
-
-
+        for(int i = 0; i < myHobbies.length; i++) {
+            if(myHobbies[i] == hobby) {
+                return true;
+            }
+        }
         return false;
     }
 }
